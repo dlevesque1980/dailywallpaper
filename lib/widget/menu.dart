74e475939a9a94bba6540024c82f1981fc754b98
Didier Levesque <didier.levesque@gmail.com>
2019-09-21 14:15:36 -0400

    update flutter 2.5

--- lib/widget/menu.dart
+++ lib/widget/menu.dart
@@ -8,18 +8,18 @@ class Menu extends StatelessWidget {
     return new PopupMenuButton<String>(
       onSelected: (choice) => Navigator.pushNamed(context, choice),
       padding: EdgeInsets.all(0.0),
+      offset: Offset.fromDirection(2, 55),
       icon: new Icon(
         Icons.more_vert,
         color: Colors.white,
       ),
       itemBuilder: (BuildContext context) => <PopupMenuItem<String>>[
-            PopupMenuItem<String>(
-              value: '/settings',
-              child: const Text('Settings', textAlign: TextAlign.center),
-              height: 40.0,
-            ),
-            PopupMenuItem<String>(value: '/older', enabled: false, child: const Text('Older wallpaper', textAlign: TextAlign.center), height: 40.0),
-          ],
+        PopupMenuItem<String>(
+          value: '/settings',
+          child: const Text('Settings', textAlign: TextAlign.center),
+          height: 40.0,
+        ),
+      ],
     );
   }
 }
